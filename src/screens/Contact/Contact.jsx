e6d5ad9d483e0e0658c9353b1b87a59e3d5acf8a
willwardlow <wardlow.william@gmail.com>
2021-04-22 13:45:45 -0500

    refactored contact page

--- src/screens/Contact/Contact.jsx
+++ src/screens/Contact/Contact.jsx
@@ -5,26 +5,27 @@ export default function Contact() {
     <div className='contact-container'>
       <div className='form-text'>
         <p>
-         Want to contact me? Fill out this form!
+          Want to contact me? Fill out this form!
+           <p className="email">
+            Or email me directly at wardlow.william@gmail.com
+           </p>
         </p>
       </div>
       <form action="https://getform.io/f/b6a30d8f-8d64-4088-98f8-cbdf8f938673" method="POST">
-        <label htmlFor='name'> Name:
+        <label htmlFor='name'> Name:</label>
           <input type='text' name='name' placeholder='Enter name'/>
-        </label>
+        
 
-        <label htmlFor='email'> Email:
+        <label htmlFor='email'> Email: </label>
           <input type='text' name='email' placeholder='Enter Email Address:' />
-        </label>
+        
 
-        <label htmlFor='message'> Message:
-          <textarea name='message' rows='10' cols='24' placeholder='Enter Message:' />
-        </label>
-
+        <label htmlFor='message'> Message:</label>
+        <textarea name='message' rows='10' cols='50' placeholder='Enter Message:' />
+        
         <input type='submit'/>
-
-
       </form>
+
       
     </div>
   )
